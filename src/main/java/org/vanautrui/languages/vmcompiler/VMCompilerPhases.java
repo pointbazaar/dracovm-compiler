ed5eba656e99fcdfc63f156e4ea8b6de05c9bd6e
pointbazaar <alex23667@gmail.com>
2019-10-27 03:02:59 +0100

    minor changes

--- src/main/java/org/vanautrui/languages/vmcompiler/VMCompilerPhases.java
+++ src/main/java/org/vanautrui/languages/vmcompiler/VMCompilerPhases.java
@@ -1,7 +1,5 @@
 package org.vanautrui.languages.vmcompiler;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.io.IOUtils;
 import org.vanautrui.languages.vmcompiler.codegenerator.AssemblyCodeGenerator;
 
 import java.nio.file.Files;
@@ -10,9 +8,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.lang.System.out;
 import static org.vanautrui.languages.vmcompiler.VMCompilerPhaseUtils.printBeginPhase;
-import static org.vanautrui.languages.vmcompiler.VMCompilerPhaseUtils.printEndPhase;
 
 public final class VMCompilerPhases {
 
@@ -35,29 +31,14 @@ public final class VMCompilerPhases {
     public static final String SEGMENT_ARG = "ARG";
     public static final String SEGMENT_LOCAL = "LOCAL";
 
-    private static final String FLAG_DEBUG="debug";
-    private static final String FLAG_TIMED="timed";
+    public VMCompilerPhases() { }
 
-    private final boolean debug;
-    private final boolean timed;
-
-    public VMCompilerPhases(final CommandLine cmd){
-        this.debug=cmd.hasOption(FLAG_DEBUG);
-        this.timed=cmd.hasOption(FLAG_TIMED);
-    }
-
-    public VMCompilerPhases() {
-        //for testing of compiler phases
-        this.debug=false;
-        this.timed=false;
-    }
-
-    public Path compile_vm_codes_and_generate_executable(final List<String> draco_vm_codes,String filename_without_extension, final boolean debug)throws Exception{
+    public synchronized Path compile_vm_codes_and_generate_executable(final List<String> draco_vm_codes,String filename_without_extension, final boolean debug)throws Exception{
         printBeginPhase("VM CODE COMPILATION",debug);
+        List<String> asm_codes = AssemblyCodeGenerator.compileVMCode(draco_vm_codes);
 
         printBeginPhase("CALL NASM",debug);
-
-        return phase_generate_executable(AssemblyCodeGenerator.compileVMCode(draco_vm_codes),filename_without_extension);
+        return phase_generate_executable(asm_codes,filename_without_extension);
     }
 
     /**
@@ -65,7 +46,7 @@ public final class VMCompilerPhases {
      *
      * @throws Exception an exception is thrown if nasm or ld exit nonzero
      */
-    private Path phase_generate_executable(final List<String> asm_codes,final String filename_without_extension) throws Exception{
+    private synchronized Path phase_generate_executable(final List<String> asm_codes,final String filename_without_extension) throws Exception{
 
         final String asm_file_name = filename_without_extension+".asm";
         final Path asm_path = Paths.get(asm_file_name);
