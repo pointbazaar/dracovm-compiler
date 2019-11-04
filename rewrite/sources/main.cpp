35b8a0628dab1d75a508c0b98ed9c729fcf87cf7
pointbazaar <alex23667@gmail.com>
2019-11-04 01:48:42 +0100

    some fluff

--- rewrite/sources/main.cpp
+++ rewrite/sources/main.cpp
@@ -6,12 +6,26 @@ int main(int argc, char* argv[]){
 
 	cout << "DRACOVM Compiler" << endl;
 
-	//TODO: parse files given as arguments
+	const unsigned int nSubroutines = argc-1;
+
+	//parse files given as arguments:
+	//they should all have ".dracovm-subroutine"
+	//extension. and the files should all exist. 
+	//so we should check that.
+	
 	cout << "printing arguments: " << endl;
 	for(int i=1;i<argc;i++){
-
-		cout << argv[i] << endl;
+		char* filename = argv[i];
+		
+		cout << " - " << filename << endl;
+		try{
+			
+		}catch(const string msg){
+			cerr << "error with " << filename << endl;
+		}
 	}
+
+	cout << "successfully compiled " << nSubroutines << " subroutines" << endl;
 	
 	return 0;
 }
