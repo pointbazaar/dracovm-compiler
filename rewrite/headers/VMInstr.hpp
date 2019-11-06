a8262974fd5ab86274d10ceef812ec0e97aec3e5
pointbazaar <alex23667@gmail.com>
2019-11-07 00:59:22 +0100

    minor changes, build fails

--- rewrite/headers/VMInstr.hpp
+++ rewrite/headers/VMInstr.hpp
@@ -1,8 +1,13 @@
-
+#include <string>
 
 using namespace std;
 
 class VMInstr{
 
-	
+	public:
+		const string cmd;
+		const string arg1; //may be null
+		const string arg2; //may be null
+		VMInstr(string str);
+		string toString();
 };
--- rewrite/sources/VMInstr.cpp
+++ rewrite/sources/VMInstr.cpp
@@ -1,2 +1,44 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include "VMInstr.hpp"
+
 using namespace std;
 
+VMInstr::VMInstr(const string str){
+	//this should split it into strings
+	//https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
+
+	istringstream iss(str);
+	vector<string> res((istream_iterator<string>(iss)),
+                                 istream_iterator<string>());
+
+	if(res.size()==0){
+		cerr << "vm command " << str 
+			<< " was empty" << endl;
+		exit(1);
+	}
+
+	if(res.size()>3){
+		cerr << "vm command " << str 
+			<< " was too long. max size is 3 words." << endl;
+	}
+
+	this->cmd=res.at(0);
+	if(res.size()>1){
+		this->arg1=res.at(1);
+	}else{
+		this->arg1=null;
+	}
+	if(res.size()>2){
+		this->arg2=res.at(2);
+	}else{
+		this->arg2=null;
+	}
+}
+
+VMInstr::toString(){
+	string a1 = (this->arg1==null)?"":this->arg1;
+	string a2 = (this->arg2==null)?"":this->arg2;
+	return this->cmd+" "+a1+" "+a2;
+}
