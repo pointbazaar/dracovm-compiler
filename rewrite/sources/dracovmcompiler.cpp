68298c46d3386d5043845b149fb3992f106b717f
pointbazaar <alex23667@gmail.com>
2019-11-06 21:39:05 +0100

    commentary

--- rewrite/sources/dracovmcompiler.cpp
+++ rewrite/sources/dracovmcompiler.cpp
@@ -6,6 +6,17 @@ using namespace std;
 
 bool compile_main(vector<string> filenames){
 	cout << "compile_main" << endl;
-	//TODO	
+
+	//TODO: from these filenames, check if each has 
+	//correct '.subroutine.dracovm' extension
+	// (marking them as a .dracovm file which only has 1 subroutine,
+	// and where the name of the subroutine is encoded in the filename) 
+	//and exists. 
+	//then make hashes, compare those to those
+	//found in 'dracovm.info' , and if they differ,
+	//add them to the list of files to be recompiled.
+
+	//TODO
+
 	return true;
 }
