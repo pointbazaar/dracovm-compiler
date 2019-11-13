9a77d5c8a48c88af08d7f2160b55f299c8b5d1b5
pointbazaar <alex23667@gmail.com>
2019-11-13 17:57:51 +0100

    work on testing framework

--- rewrite/maintest.cpp
+++ rewrite/maintest.cpp
@@ -7,9 +7,47 @@
 
 using namespace std;
 
+bool test_fadd1();
+
 int main(int argc, char* argv[]){
 	
 	cout << "START TESTS" << endl;
 	//TODO:
+
+	vector<bool> test_results = {
+		test_fadd1()
+	};
+
+	for(bool x : test_results){
+		if(x){
+			cout << "PASS " << endl;
+		}else{
+			cout << "FAIL " << endl;
+		}
+	}
+	
 	return 0;
+}
+
+bool test_fadd1(){
+
+	const vector<string> vmcodes = {
+		"subroutine Main_main 0 args 0 locals",
+		"fconst 3.0",
+		"fconst 2.0",
+		"fadd",
+		"flt",
+		"if-goto exit0",
+		"iconst 1",
+		"exit",
+		"label exit0",
+		"iconst 0",
+		"exit"
+	};
+
+	//start a process, and examine its exit value
+	int status=1;
+	status = system("./test_fadd1");
+
+	return status==0;
 }
